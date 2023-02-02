893d95eb64eb29920b4c42e2da2b310620d6c6e2
RPE8 <vitali.shuman96@gmail.com>
2023-02-02 07:41:29 +0300

    Add dummy logic for working woth DB

--- part-2d/server/server.js
+++ part-2d/server/server.js
@@ -1,15 +1,32 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["*"]);
-  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.append("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb://127.0.0.1/phoneBookDB";
 
-app.listen(3001, function () {
-  console.log("listening on 3001");
-});
+async function main() {
+  await mongoose.connect(mongoDB);
+  app.use((req, res, next) => {
+    res.append("Access-Control-Allow-Origin", ["*"]);
+    res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.append("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
+  app.listen(3001, function () {
+    console.log("listening on 3001");
+  });
+}
+
+main()
+  .then(async () => {
+    const numbers = new mongoose.Schema({
+      name: String,
+      number: Number,
+    });
+    const Line = mongoose.model("Line", numbers);
+    const john = new Line({ name: "John", number: 123 });
+    await john.save();
+  })
+  .catch((err) => console.log(err));
