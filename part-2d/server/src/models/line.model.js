92f3e8fedf24886983cdca51f2275ece54f58091
RPE8 <vitali.shuman96@gmail.com>
2023-02-04 15:16:35 +0300

    Add custom toJSON on mongoose model

--- part-2d/server/src/models/line.model.js
+++ part-2d/server/src/models/line.model.js
@@ -8,6 +8,13 @@ const LineSchema = new mongoose.Schema({
   number: Number,
 });
 
+LineSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject.__v;
+  },
+});
+
 const Line = mongoose.model("Line", LineSchema);
 
 module.exports = {
