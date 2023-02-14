d66620c2d6f50433b68ddfc6953702b4402606cc
RPE8 <vitali.shuman96@gmail.com>
2023-02-14 18:56:25 +0300

    Fix error handler middleware by accepting CORRECT params

--- part-4/backend/src/middlewares/error.middleware.js
+++ part-4/backend/src/middlewares/error.middleware.js
@@ -1,5 +1,4 @@
-const errorHandler = (err, req, res) => {
-  console.log("ERR");
+const errorHandler = (err, req, res, next) => {
   const statusCode = (res.statusCode !== 200 && res.statusCode) || 500;
   res.status(statusCode);
 
