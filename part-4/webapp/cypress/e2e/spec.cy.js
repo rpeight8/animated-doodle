04eaf822a16abf453f42bd72e710aad243ed4df6
RPE8 <vitali.shuman96@gmail.com>
2023-02-25 20:28:23 +0300

    5.17 - 5.23

--- part-4/webapp/cypress/e2e/spec.cy.js
+++ part-4/webapp/cypress/e2e/spec.cy.js
@@ -32,6 +32,14 @@ describe("Blogs App", () => {
 
   describe("when logged in", () => {
     beforeEach(() => {
+      cy.visit("");
+      cy.request("POST", `${Cypress.env("BACKEND_URL")}/api/testing`);
+      const user = {
+        name: "test",
+        username: "test",
+        password: "password1337",
+      };
+      cy.request("POST", `${Cypress.env("BACKEND_URL")}/api/users/`, user);
       cy.login({ username: "test", password: "password1337" });
     });
 
@@ -50,6 +58,27 @@ describe("Blogs App", () => {
       cy.get(".blog-list-item__title")
         .contains("a title created by cypress")
         .should("not.exist");
+    });
+
+    it("a nre blog can be liked and disliked", async () => {
+      cy.contains("Add blog").click();
+      cy.get("input[placeholder='title']").type("a title created by cypress");
+      cy.get("input[placeholder='author']").type("cypres");
+      cy.get("input[placeholder='url']").type("cypres-url");
+      cy.contains(/^add$/).click();
+      cy.get(".blog-list-item__title").contains("a title created by cypress");
+      cy.contains("Details").click();
+      cy.contains("Votes: 0");
+      cy.contains(/^Vote$/).click();
+      cy.contains("Votes: 1");
+      cy.contains(/^Vote$/).click();
+      cy.contains("Votes: 2");
+      cy.contains("Remove Vote").click();
+      cy.contains("Votes: 1");
+      cy.contains("Remove Vote").click();
+      cy.contains("Votes: 0");
+      cy.contains("Remove Vote").click();
+      cy.contains("Votes: 0");
     });
   });
 });
