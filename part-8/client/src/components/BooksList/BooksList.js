733b23cef6faeef571846c8a1574760aa365c67e
RPE8 <vitali.shuman96@gmail.com>
2023-03-12 19:19:29 +0300

    Initial edit

--- part-8/client/src/components/BooksList/BooksList.js
+++ part-8/client/src/components/BooksList/BooksList.js
@@ -1,12 +1,14 @@
 import Table from "react-bootstrap/Table";
-import { Form } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import { Button } from "react-bootstrap";
 import { FIND_BOOKS } from "../../queries";
 import { useQuery } from "@apollo/client";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import LibraryContext from "../../LibraryContext";
 
 function BooksList() {
   const [state, _] = useContext(LibraryContext);
+  const [editBook, setEditBook] = useState(null);
 
   const result = useQuery(FIND_BOOKS, {
     variables: { title: state.bookSearchString },
@@ -26,14 +28,63 @@ function BooksList() {
             <th>#</th>
             <th>Title</th>
             <th>Author</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
           {books.map((book, index) => (
             <tr key={book.id}>
               <td>{index + 1}</td>
-              <td>{book.title}</td>
-              <td>{book.author}</td>
+              {!editBook ||
+                (editBook !== book.id && (
+                  <>
+                    <td>{book.title}</td>
+                    <td>{book.author}</td>
+                    <td>
+                      <Button
+                        variant="light"
+                        size="sm"
+                        onClick={() => {
+                          setEditBook(book.id);
+                        }}
+                      >
+                        Edit
+                      </Button>
+                      <Button variant="light" size="sm">
+                        Delete
+                      </Button>
+                    </td>
+                  </>
+                )) || (
+                  <>
+                    <td>
+                      <Form.Control type="text"></Form.Control>
+                    </td>
+                    <td>
+                      <Form.Control type="text"></Form.Control>
+                    </td>
+                    <td>
+                      <Button
+                        variant="light"
+                        size="sm"
+                        onClick={() => {
+                          setEditBook(null);
+                        }}
+                      >
+                        Save
+                      </Button>
+                      <Button
+                        variant="light"
+                        size="sm"
+                        onClick={() => {
+                          setEditBook(null);
+                        }}
+                      >
+                        Cancel
+                      </Button>
+                    </td>
+                  </>
+                )}
             </tr>
           ))}
         </tbody>
