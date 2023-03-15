fdccb03ede5d4e7d6a88cbcaf03d0a69133855e3
RPE8 <vitali.shuman96@gmail.com>
2023-03-15 15:05:27 +0300

    Part-9b 9.1 - 9.2

---
+++ part-9/b/src/exerciseCalculator.ts
@@ -0,0 +1,42 @@
+type Result = {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+};
+
+function calculateExercises(dailyExercises: number[], target: number): Result {
+  if (
+    !Array.isArray(dailyExercises) ||
+    dailyExercises.some(isNaN) ||
+    !target ||
+    isNaN(target)
+  ) {
+    throw new Error("malformatted parameters");
+  }
+
+  const average =
+    dailyExercises.reduce((a, b) => a + b, 0) / dailyExercises.length;
+  const rating = average < target ? 1 : average === target ? 2 : 3;
+  const ratingDescription =
+    rating === 1
+      ? "not too bad but could be better"
+      : rating === 2
+      ? "not too bad but could be better"
+      : "good job";
+
+  return {
+    periodLength: dailyExercises.length,
+    trainingDays: dailyExercises.filter((value) => value > 0).length,
+    success: average >= target,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
