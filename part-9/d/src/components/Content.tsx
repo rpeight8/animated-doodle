9b04d78e0e3dd7a322cf56e3afe9161bcc0b2a38
RPE8 <vitali.shuman96@gmail.com>
2023-03-21 13:25:14 +0300

    Init next app

--- part-9/d/src/components/Content.tsx
+++ part-9/d/src/components/Content.tsx
@@ -1,13 +1,39 @@
-import { Topic } from "../types";
+import { CoursePart } from "../types";
 
-function Content({ topics }: { topics: Topic[] }) {
+function Content({ topics }: { topics: CoursePart[] }) {
   return (
     <div>
-      {topics.map((topic, i) => (
-        <p key={i}>
-          {topic.name} {topic.exerciseCount}
-        </p>
-      ))}
+      {topics.map((topic) => {
+        switch (topic.kind) {
+          case "basic":
+            return (
+              <div key={topic.name}>
+                <h3>{topic.name}</h3>
+                <p>{topic.description}</p>
+                <p>Exercises: {topic.exerciseCount}</p>
+              </div>
+            );
+          case "group":
+            return (
+              <div key={topic.name}>
+                <h3>{topic.name}</h3>
+                <p>Exercises: {topic.exerciseCount}</p>
+                <p>Group projects: {topic.groupProjectCount}</p>
+              </div>
+            );
+          case "background":
+            return (
+              <div key={topic.name}>
+                <h3>{topic.name}</h3>
+                <p>{topic.description}</p>
+                <p>Exercises: {topic.exerciseCount}</p>
+                <p>Read: {topic.backroundMaterial}</p>
+              </div>
+            );
+          default:
+            return null;
+        }
+      })}
     </div>
   );
 }
--- part-9/d/src/types.ts
+++ part-9/d/src/types.ts
@@ -1,4 +1,25 @@
-export type Topic = {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
-};
+}
+
+interface CoursePartBasic extends CoursePartBase {
+  description: string;
+  kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+interface CoursePartBackround extends CoursePartBase {
+  description: string;
+  backroundMaterial: string;
+  kind: "background";
+}
+
+export type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackround;
